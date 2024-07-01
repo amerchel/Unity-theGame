f4f973258ccc2b024b6ed83db7ec3cf3f710e43b
amerchel <ada.merchel98@gmail.com>
2024-07-01 13:36:59 +0200

    Added player meele attack and jump

--- Assets/Scripts/PlayerControler.cs
+++ Assets/Scripts/PlayerControler.cs
@@ -1,43 +1,72 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Rendering.LookDev;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-
+[RequireComponent(typeof(Rigidbody2D), typeof(TouchingDirections))]
 public class PlayerControler : MonoBehaviour
 {
     public float walkSpeed = 5f;
     public float runSpeed = 8f;
+    public float airWalkSpeed = 3f;
+    public float jumpImpulse = 10f;
     Vector2 moveInput;
+    private TouchingDirections touchingDirections;
 
-    public float CurrentMoveSpeed { get
+    public float CurrentMoveSpeed
+    {
+        get
         {
-            if(IsMoving)
+            if (CanMove)
             {
-                if(IsRunning)
+                if (IsMoving && !touchingDirections.IsOnWall)
                 {
-                    return runSpeed;
-                } else {
-                    return walkSpeed;
+                    if (touchingDirections.IsGrounded)
+                    {
+                        if (IsRunning)
+                        {
+                            return runSpeed;
+                        }
+                        else
+                        {
+                            return walkSpeed;
+                        }
+                    }
+                    else
+                    {
+                        // Air Move
+                        return airWalkSpeed;
+                    }
                 }
-            } else
+                else
+                {
+                    // Idle speed = 0
+                    return 0;
+                }
+            }
+            else
             {
-                // Idle speed = 0
+                // Movement locked
                 return 0;
             }
+
         }
     }
 
     [SerializeField]
     private bool _isMoving = false;
 
-    public bool IsMoving {get
+    public bool IsMoving
     {
-        return _isMoving;
-    } private set
+        get
         {
-        _isMoving = value;
-        animator.SetBool("isMoving", value);
+            return _isMoving;
+        }
+        private set
+        {
+            _isMoving = value;
+            animator.SetBool("isMoving", value);
         }
     }
 
@@ -59,16 +88,29 @@ public class PlayerControler : MonoBehaviour
 
     public bool _isFacingRight = true;
 
-    public bool IsFacingRight {get { return _isFacingRight; } private set {
-            if(_isFacingRight != value)
+    public bool IsFacingRight
+    {
+        get { return _isFacingRight; }
+        private set
+        {
+            if (_isFacingRight != value)
             {
                 // Flip the local scale to make the player face the opposite direction
                 transform.localScale *= new Vector2(-1, 1);
             }
-            
-            _isFacingRight = value; 
-            
-    } }
+
+            _isFacingRight = value;
+
+        }
+    }
+
+    public bool CanMove
+    {
+        get
+        {
+            return animator.GetBool("canMove");
+        }
+    }
 
     Rigidbody2D rb;
     Animator animator;
@@ -77,24 +119,27 @@ public class PlayerControler : MonoBehaviour
     {
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
+        touchingDirections = GetComponent<TouchingDirections>();
     }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     private void FixedUpdate()
     {
         rb.velocity = new Vector2(moveInput.x * CurrentMoveSpeed, rb.velocity.y);
+
+        animator.SetFloat("yVelocity", rb.velocity.y);
     }
-    
+
     public void OnMove(InputAction.CallbackContext context)
     {
         moveInput = context.ReadValue<Vector2>();
@@ -106,11 +151,12 @@ public class PlayerControler : MonoBehaviour
 
     private void SetFacingDirection(Vector2 moveInput)
     {
-        if(moveInput.x > 0 && !IsFacingRight)
+        if (moveInput.x > 0 && !IsFacingRight)
         {
             //Face the right
             IsFacingRight = true;
-        } else if (moveInput.x < 0 && IsFacingRight)
+        }
+        else if (moveInput.x < 0 && IsFacingRight)
         {
             //Face the left
             IsFacingRight = false;
@@ -119,12 +165,30 @@ public class PlayerControler : MonoBehaviour
 
     public void OnRun(InputAction.CallbackContext context)
     {
-        if(context.started)
+        if (context.started)
         {
             IsRunning = true;
-        } else if(context.canceled)
+        }
+        else if (context.canceled)
         {
             IsRunning = false;
+        }
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        if (context.started && touchingDirections.IsGrounded && CanMove)
+        {
+            animator.SetTrigger("jump");
+            rb.velocity = new Vector2(rb.velocity.x, jumpImpulse);
+        }
+    }
+
+    public void OnAttack(InputAction.CallbackContext context)
+    {
+        if (context.started)
+        {
+            animator.SetTrigger("attack");
         }
     }
 }
---
+++ Assets/Scripts/StateMachine/SetBoolBoolBehaviour.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetBoolBoolBehaviour : StateMachineBehaviour
+{
+    public string boolName;
+    public bool updateOnState;
+    public bool updateOnStateMachine;
+    public bool valueOnEnter, valueOnExit;
+
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        animator.SetBool(boolName, valueOnEnter);
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    //override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    
+    //}
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        animator.SetBool(boolName, valueOnExit);
+    }
+
+    // OnStateMove is called right after Animator.OnAnimatorMove()
+    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that processes and affects root motion
+    //}
+
+    // OnStateIK is called right after Animator.OnAnimatorIK()
+    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that sets up animation IK (inverse kinematics)
+    //}
+
+    //OnStateMachineEnter is called when entering a state machine via its Entry Node
+    public override void OnStateMachineEnter(Animator animator, int stateMachinePathHash)
+    {
+        if (updateOnStateMachine)
+            animator.SetBool(boolName, valueOnEnter);
+    }
+
+    //OnStateMachineExit is called when exiting a state machine via its Exit Node
+    public override void OnStateMachineExit(Animator animator, int stateMachinePathHash)
+    {
+        if (updateOnStateMachine)
+            animator.SetBool(boolName, valueOnExit);
+    }
+}
