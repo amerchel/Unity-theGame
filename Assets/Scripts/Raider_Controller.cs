facad43df227484e00d6bf8f5467ed4a0252d0f0
Cr45h7 <p0771122@gmail.com>
2024-07-07 16:10:35 +0200

    Merged fixmap & roll
    Merged fixmap & roll

---
+++ Assets/Scripts/Raider_Controller.cs
@@ -0,0 +1,133 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices.WindowsRuntime;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D), typeof(TouchingDirections), typeof(Damageable))]
+
+public class Raider_Controller : MonoBehaviour
+{
+    public float walkSpeed = 3f;
+    public float walkStopRate = 0.05f;
+    public DetectionZone attackZone;
+    public DetectionZone cliffDetectionZone;
+
+    Rigidbody2D rb;
+    TouchingDirections touchingDirections;
+    Animator animator;
+    Damageable damageable;
+
+    public enum WalkableDirection { Right, Left }
+
+    private WalkableDirection _walkDirection;
+    private Vector2 walkDirectionVector = Vector2.right;
+
+    public WalkableDirection WalkDirection
+    {
+        get { return _walkDirection; }
+        set
+        {
+            if (_walkDirection != value)
+            {
+                //Direction flipped
+                gameObject.transform.localScale = new Vector2(gameObject.transform.localScale.x * -1, gameObject.transform.localScale.y);
+
+                if (value == WalkableDirection.Right)
+                {
+                    walkDirectionVector = Vector2.right;
+                }
+                else if (value == WalkableDirection.Left)
+                {
+                    walkDirectionVector = Vector2.left;
+                }
+            }
+
+            _walkDirection = value;
+        }
+    }
+
+    public bool _hasTarget = false;
+
+    public bool HastTarget
+    {
+        get { return _hasTarget; }
+        private set
+        {
+            _hasTarget = value;
+            animator.SetBool("hasTarget", value);
+        }
+    }
+
+    public bool CanMove
+    {
+        get
+        {
+            return animator.GetBool("canMove");
+        }
+    }
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        touchingDirections = GetComponent<TouchingDirections>();
+        animator = GetComponent<Animator>();
+        damageable = GetComponent<Damageable>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HastTarget = attackZone.detectedColliders.Count > 0;
+    }
+
+    private void FixedUpdate()
+{
+
+    if (touchingDirections.IsGrounded && touchingDirections.IsOnWall)
+    {
+        FlipDirection();
+    }
+
+    if (!damageable.LockVelocity)
+    {
+        if (CanMove && touchingDirections.IsGrounded)
+        {
+            rb.velocity = new Vector2(walkSpeed * walkDirectionVector.x, rb.velocity.y);
+        }
+        else
+        {
+            rb.velocity = new Vector2(Mathf.Lerp(rb.velocity.x, 0, walkStopRate), rb.velocity.y);
+        }
+    }
+}
+ private void FlipDirection()
+    {
+        if (WalkDirection == WalkableDirection.Right)
+        {
+            WalkDirection = WalkableDirection.Left;
+        }
+        else if (WalkDirection == WalkableDirection.Left)
+        {
+            WalkDirection = WalkableDirection.Right;
+        }
+        else
+        {
+            Debug.LogError("Current walkable direction is not set to legal values of right or left");
+        }
+    }
+
+public void OnHit(int damage, Vector2 knockback)
+{
+    rb.velocity = new Vector2(knockback.x, rb.velocity.y + knockback.y);
+}
+
+    public void OnCliffDetected()
+    {
+        if(touchingDirections.IsGrounded)
+        {
+            FlipDirection();
+        }
+    }
+
+}
---
+++ Assets/Scripts/attack.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class attack : MonoBehaviour
+{
+    public int attackDamage = 10;
+    public Vector2 knockback = Vector2.zero;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // See if it can be hit
+        Damageable damageable = collision.GetComponent<Damageable>();
+
+        if (damageable != null)
+        {
+            Vector2 deliverKnockback = transform.parent.localScale.x > 0 ? knockback : new Vector2(-knockback.x, knockback.y);
+
+            // Hit the target
+            bool gotHit = damageable.Hit(attackDamage, deliverKnockback);
+
+            if (gotHit)
+                Debug.Log(collision.name + "hit for " + attackDamage);
+        }
+    }
+}
