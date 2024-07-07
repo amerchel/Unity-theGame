40e9becb0a9773a610100f1f3f8b49a259aa872d
Adrian Bieniek <a.bieniek.kontakt@gmail.com>
2024-07-07 17:27:57 +0200

    add pause menu

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -3,41 +3,20 @@ using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
-    [SerializeField] GameObject pauseMenu;
-
-    private bool isPaused = false;
-
-    void Update()
+    public void PauseGame()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
-        {
-            if (isPaused)
-            {
-                UnpauseGame();
-            }
-            else
-            {
-                PauseGame();
-            }
-        }
+        SceneManager.LoadScene(3);
+        Time.timeScale = 0;
     }
 
     public void UnpauseGame()
     {
-        pauseMenu.SetActive(false);
         Time.timeScale = 1;
-        isPaused = false;
-    }
-
-    public void PauseGame()
-    {
-        pauseMenu.SetActive(true);
-        Time.timeScale = 0;
-        isPaused = true;
+        SceneManager.LoadScene(1);
     }
 
     public void EndGame()
     {
-        SceneManager.LoadScene(1);
+        SceneManager.LoadScene(0);
     }
 }
