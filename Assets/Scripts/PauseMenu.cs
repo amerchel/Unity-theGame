09255f9795eae265c7cdf47243ffeeb4b75e6714
Cr45h7 <p0771122@gmail.com>
2024-07-07 18:48:41 +0200

    Added sounds & end
    Added sounds & end

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public void PauseGame()
+    {
+        SceneManager.LoadScene(3);
+        Time.timeScale = 0;
+    }
+
+    public void UnpauseGame()
+    {
+        SceneManager.LoadScene(1);
+        Time.timeScale = 1;
+    }
+
+    public void EndGame()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
